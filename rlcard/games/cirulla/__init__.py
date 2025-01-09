da18e7dc06377632224647478544844fd56d78e0
lorenzo <farinalorenzo4@gmail.com>
2025-01-09 16:17:49 +0100

    updated init file with Cirulla stuff, even if idk why :), no round.py

--- rlcard/games/cirulla/__init__.py
+++ rlcard/games/cirulla/__init__.py
@@ -1,6 +1,4 @@
-from rlcard.games.uno.dealer import UnoDealer as Dealer
-from rlcard.games.uno.judger import UnoJudger as Judger
-from rlcard.games.uno.player import UnoPlayer as Player
-from rlcard.games.uno.round import UnoRound as Round
-from rlcard.games.uno.game import UnoGame as Game
-
+from rlcard.games.cirulla.dealer import CirullaDealer as Dealer
+from rlcard.games.cirulla.judger import CirullaJudger as Judger
+from rlcard.games.cirulla.player import CirullaPlayer as Player
+from rlcard.games.cirulla.game import CirullaGame as Game
